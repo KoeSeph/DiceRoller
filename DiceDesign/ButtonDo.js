3835c40da797ad7b1941009625f1cd47a909f9ed
Francisco Valdes-Jasso <koesavant@gmail.com>
2015-01-07 20:47:01 -0600

    Making it rain
    Its buttons. Not real rain.

--- DiceDesign/ButtonDo.js
+++ DiceDesign/ButtonDo.js
@@ -1,34 +1,47 @@
 $(document).ready(function(){
 
-$('#upSelect').change(function() {
-   $('#chicken').removeClass('noodle');
-  if($("#upSelect").val() == 'blu') {
-     $('#baseDiv').css('background-color', 'blue');
-  } else if ($("#upSelect").val() == 'red') {
-    $('#baseDiv').css('background-color', 'red');
-  } else {
-    $('#baseDiv').css('background-color', '#000');
-  };
-  console.log($("#upSelect").val());
-});
-  $('#upSelectt').change(function() {
-   $('#chickent').removeClass('noodlet');
-  if($("#upSelectt").val() == 'blu') {
-     $('#baseDivt').css('background-color', 'blue');
-  } else if ($("#upSelectt").val() == 'red') {
-    $('#baseDivt').css('background-color', 'red');
-  } else {
-    $('#baseDivt').css('background-color', '#000');
-  };
-  console.log($("#upSelectt").val());
-});
-  $('#chicken').on('mouseout', function(){
-    $('#chicken').addClass('noodle');
+   $('select').change(function() {
+    decide($(this));
   });
-  $('#chickent').on('mouseout', function(){
-    $('#chickent').addClass('noodlet');
+
+  /* 
+  changes the background color of the sandwiched divs
+  logs the color changed to
+  ARGUMENTS:
+      elem - the select element that received the action
+  */
+  var decide = function(elem) {
+    var baseDiv = elem.parent();
+    var chicken = baseDiv.parent();
+    chicken.removeClass('noodle');
+    switch(elem.val()){
+        case 'd2':
+        baseDiv.prev().css('background-color', 'blue');
+        break;
+        case 'd4':
+        baseDiv.prev().css('background-color', 'red');
+        break;
+        case 'd6':
+        baseDiv.prev().css('background-color', 'black');
+        break;
+    };
+    baseDiv.prev().css('border', 'none');
+    baseDiv.prev().css('padding', '4px');
+    console.log(elem.val());
+  };
+  $(".hook").on('mouseout', function(){
+    $(this).addClass('noodle');
   });
-  
-  /*$("#upSelect").val()*/
+  $('.pushin').on('click', function(){
+    $('.wrapper').append("<div class='thinger'> <div class='hook noodle'>  <div id='baseDiv'><p class='stubborn'>Hover me</p></div>  <div id='upDiv'>    <select class='select'>        <option value='blu'>blu</option>        <option value='red'>red</option>        <option value='blk'>blk</option>    </select>  </div> </div></div>");
+     $('select').change(function() {
+    decide($(this));
+  });
+    $(".hook").on('mouseout', function(){
+    $(this).addClass('noodle');
+  });
+    console.log('work');
+  });
+  /*$(select).val()*/
   
 });
